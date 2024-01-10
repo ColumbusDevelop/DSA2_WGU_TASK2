85b2d6cf0fe3f8a3d689a40c7bd88e62e5817166
Hedonium <hedonizing@gmail.com>
2024-01-10 09:37:18 -0500

    Add initial Hash Table implementation

--- main.py
+++ main.py
@@ -36,7 +36,61 @@ with open("distance.csv") as distanceFile:
 
 ### Hash Table Class
 
+# Referencing C950 zyBooks: Figure 7.8.2: Hash table using chaining
 
+# Hash Table implementation
+
+class HashTable:
+    def __init__(self, initial_capacity = 40):
+        self.hashtable = []
+        for number in range(initial_capacity):
+            self.hashtable.append([])
+
+    # Adds a new item into the hash table and updates an existing item in the array.
+
+    def add(self, hash_key, hash_item):
+        hashbucket = hash(hash_key) % len(self.hashtable)
+        hashbucket_array = self.hashtable[hashbucket]
+
+        # If the key is already present in the hash table bucket, proceed to update it.
+
+        for keyvaluepair in hashbucket_array:
+
+            if keyvaluepair[0] == hash_key:
+                keyvaluepair[1] = hash_item
+                return True
+
+        # If the item is not present in the bucket, append it to the end of the array.
+ 
+        key_value = [hash_key, hash_item]
+        hashbucket_array.append(key_value)
+        return True
+
+    # Gets the hash table corresponding to an item with a matching key.
+    # If found, it will return the item; otherwise, it will return None.
+
+    def retrieve(self, hash_key):
+        hashbucket = hash(hash_key) % len(self.hashtable)
+        hashbucket_array = self.hashtable[hashbucket]
+
+        # Fetch the key from the bucket
+
+        for keyvaluepair in hashbucket_array:
+
+            if keyvaluepair[0] == hash_key:
+                return keyvaluepair[1]  # Value of keyvaluepair
+        return None 
+        
+    # Deletes an entry with a key that matches from the hash table.
+
+    def remove(self, hash_key):
+        hashbucket = hash(hash_key) % len(self.hashtable)
+        hashbucket_array = self.hashtable[hashbucket]
+
+        # If the item is present, it will be removed.
+
+        if hash_key in hashbucket_array:
+            hashbucket_array.remove(hash_key)
 
 ### Package Class
 
